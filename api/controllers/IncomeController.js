25b0b1682086b505118bd7454df255aebd6250ca
saptarsi1 <saptarsighosh1991@gmail.com>
2015-10-07 23:28:20 +0530

    added income model

---
+++ api/controllers/IncomeController.js
@@ -0,0 +1,11 @@
+/**
+ * IncomeController
+ *
+ * @description :: Server-side logic for managing incomes
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
